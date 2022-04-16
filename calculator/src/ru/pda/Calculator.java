41f965381668e1258f16fd5079dbb76dbbf515a2
FlyWildCat <100592877+FlyWildCat@users.noreply.github.com>
2022-04-16 23:38:08 +0300

    Add files via upload

--- calculator/src/ru/pda/Calculator.java
+++ calculator/src/ru/pda/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : y; //добавил проверку при делении на 0
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1; // не увидел ошибки, при х=5 вернет 5, при х=0 вернет 0, при х=-5 вернет 5
